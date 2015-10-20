ab25bbaa9d1560d999d2df776d6dfd5d8d3b4db1
Michael Barre <mbarre@artandcraftentertainment.com>
2015-10-20 10:52:57 -0500

    Now has same functionality as before, but with tabs

--- Whiteboard/MainWindow.xaml.cs
+++ Whiteboard/MainWindow.xaml.cs
@@ -21,30 +21,69 @@ namespace Whiteboard
     public partial class MainWindow : Window
     {
         Polyline currentLine;
+        int newTabCount = 1;
 
         public MainWindow()
         {
             InitializeComponent();
         }
-        void StartConnectingDots(object sender, MouseEventArgs mouse)
+        void StartConnectingDots(object sender, RoutedEventArgs e)
         {
-            Point mousePoint = mouse.GetPosition(null);
-            currentLine = new Polyline
-            {
-                Stroke = Brushes.Black,
-                StrokeThickness = 2
-            };
-            Canvas canvas = (Canvas)sender;
-            canvas.Children.Add(currentLine);
-            currentLine.Points.Add(mousePoint);
-        }
-        void ConnectTheDots(object sender, MouseEventArgs mouse)
-        {
+            MouseEventArgs mouse = (MouseEventArgs)e;
             if (mouse.LeftButton == MouseButtonState.Pressed)
             {
-                Point mousePoint = mouse.GetPosition(null);
+                Point mousePoint = mouse.GetPosition((IInputElement)sender);
+                currentLine = new Polyline
+                {
+                    Stroke = Brushes.Black,
+                    StrokeThickness = 2
+                };
+                Canvas canvas = (Canvas)sender;
+                canvas.Children.Add(currentLine);
                 currentLine.Points.Add(mousePoint);
             }
+        }
+        void ConnectTheDots(object sender, RoutedEventArgs e)
+        {
+            MouseEventArgs mouse = (MouseEventArgs)e;
+            if (mouse.LeftButton == MouseButtonState.Pressed)
+            {
+                Point mousePoint = mouse.GetPosition((IInputElement)sender);
+                currentLine.Points.Add(mousePoint);
+            }
+        }
+        void CloseTabFromHeader(object sender, RoutedEventArgs e)
+        {
+            MouseEventArgs mouse = (MouseEventArgs)e;
+            if (mouse.MiddleButton == MouseButtonState.Pressed)
+            {
+                TabItem parent = (TabItem)((Control)sender).Parent;
+                TabControl grandParent = (TabControl)parent.Parent;
+                grandParent.Items.Remove(parent);
+            }
+        }
+
+        void Menu_Exit(object sender, EventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        void Menu_New(object sender, EventArgs e)
+        {
+            TabItem newTab = new TabItem();
+            Label headerLabel = new Label();
+            headerLabel.Content = String.Format("Board {0}", newTabCount);
+            headerLabel.AddHandler(Mouse.MouseDownEvent, new RoutedEventHandler(CloseTabFromHeader));
+            newTab.Header = headerLabel;
+
+            Canvas tabCanvas = new Canvas();
+            tabCanvas.Background = Brushes.Transparent;
+            tabCanvas.AddHandler(Mouse.MouseDownEvent, new RoutedEventHandler(StartConnectingDots));
+            tabCanvas.AddHandler(Mouse.MouseMoveEvent, new RoutedEventHandler(ConnectTheDots));
+            newTab.Content = tabCanvas;
+            newTabCount++;
+            tabController.Items.Add(newTab);
+            tabController.SelectedItem = newTab;
         }
     }
 }
