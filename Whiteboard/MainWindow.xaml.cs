3336b4f12d10e139cd212262e89b36546e353873
Michael Barre <mbarre@artandcraftentertainment.com>
2015-10-20 10:08:14 -0500

    Now using polylines instead of individual lines

--- Whiteboard/MainWindow.xaml.cs
+++ Whiteboard/MainWindow.xaml.cs
@@ -20,33 +20,30 @@ namespace Whiteboard
     /// </summary>
     public partial class MainWindow : Window
     {
-        Point mousePoint = new Point(-1, -1);
+        Polyline currentLine;
+
         public MainWindow()
         {
             InitializeComponent();
         }
         void StartConnectingDots(object sender, MouseEventArgs mouse)
         {
-            mousePoint = mouse.GetPosition(null);
+            Point mousePoint = mouse.GetPosition(null);
+            currentLine = new Polyline
+            {
+                Stroke = Brushes.Black,
+                StrokeThickness = 2
+            };
+            Canvas canvas = (Canvas)sender;
+            canvas.Children.Add(currentLine);
+            currentLine.Points.Add(mousePoint);
         }
         void ConnectTheDots(object sender, MouseEventArgs mouse)
         {
             if (mouse.LeftButton == MouseButtonState.Pressed)
             {
-                Canvas currentCanvas = (Canvas)sender;
-                double x = mouse.GetPosition(null).X;
-                double y = mouse.GetPosition(null).Y;
-                Line l = new Line
-                {
-                    X1 = mousePoint.X,
-                    X2 = x,
-                    Y1 = mousePoint.Y,
-                    Y2 = y,
-                    Stroke = Brushes.Black,
-                    StrokeThickness = 2
-                };
-                currentCanvas.Children.Add(l);
-                mousePoint = new Point(x, y);
+                Point mousePoint = mouse.GetPosition(null);
+                currentLine.Points.Add(mousePoint);
             }
         }
     }
